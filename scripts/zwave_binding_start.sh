d072fa5385bba43bd75474a12b57b7d4984de8c3
Aaron <aaron832@gmail.com>
2018-08-12 11:42:31 -0700

    start and stop zwave binding through rules.  Has issues.

---
+++ scripts/zwave_binding_start.sh
@@ -0,0 +1,1 @@
+sshpass -p 'habopen' ssh -p 8101 openhab@localhost 'bundle:restart org.openhab.binding.zwave'
---
+++ scripts/zwave_binding_stop.sh
@@ -0,0 +1,1 @@
+sshpass -p 'habopen' ssh -p 8101 openhab@localhost 'bundle:stop org.openhab.binding.zwave'
